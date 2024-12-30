f2e544a52857d4daf55ae89b189f2c879a6fae06
alexey.zh <mailbox.stdio@gmail.com>
2024-12-30 18:48:52 +0500

    first stable version (without subst by now, just proxying kubectl args properly)

--- main.go
+++ main.go
@@ -3,61 +3,37 @@ package main
 import (
 	"fmt"
 	"github.com/hashmap-kz/kubectl-envsubst/pkg/util"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/util/yaml"
 	"log"
 	"os"
+	"strings"
 )
 
-func complex() {
-	file := os.Args[1]
-	client, err := util.GetClient()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	readFile, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer readFile.Close()
-
-	decoder := yaml.NewYAMLOrJSONDecoder(readFile, 4096)
-	for {
-		obj := &unstructured.Unstructured{}
-		if err := decoder.Decode(obj); err != nil {
-			break
-		}
-		if err := client.Apply(obj); err != nil {
-			log.Fatal(fmt.Errorf("failed to apply resource: %w", err))
-		}
-	}
-
-}
-
-func simple() {
+func main() {
 	flags, err := util.ParseCmdFlags(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	part1 := []string{}
-	part1 = append(part1, "--namespace")
-	part1 = append(part1, flags.Namespace)
 	for _, f := range flags.Filenames {
-		part1 = append(part1, "-f")
-		part1 = append(part1, f)
+		// prepare kubectl args
+		args := []string{}
+		args = append(args, flags.Others...)
+		args = append(args, "-f")
+		args = append(args, "-")
+
+		// get file data
+		file, err := os.ReadFile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// pass to stdin
+		cmd, err := util.ExecWithStdin("kubectl", file, args...)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(strings.TrimSpace(cmd.StdoutContent))
 	}
-	part1 = append(part1, flags.Others...)
 
-	cmd, err := util.ExecCmd("kubectl", part1...)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(cmd.CombinedOutput())
-}
-
-func main() {
-	simple()
 }
--- pkg/util/execs.go
+++ pkg/util/execs.go
@@ -42,3 +42,40 @@ func ExecCmd(name string, arg ...string) (ExecCmdInternalResult, error) {
 func (e ExecCmdInternalResult) CombinedOutput() string {
 	return fmt.Sprintf("%s\n%s", e.StdoutContent, e.StderrContent)
 }
+
+func ExecWithStdin(name string, stdinContent []byte, arg ...string) (ExecCmdInternalResult, error) {
+	// Define the command to execute
+	cmd := exec.Command(name, arg...)
+
+	// Create a pipe for stdin
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return ExecCmdInternalResult{}, err
+	}
+
+	// Buffers to capture stdout and stderr
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		return ExecCmdInternalResult{}, err
+	}
+
+	// Write to stdin in a separate goroutine
+	go func() {
+		defer stdin.Close()
+		stdin.Write(stdinContent)
+	}()
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		return ExecCmdInternalResult{}, err
+	}
+
+	return ExecCmdInternalResult{
+		StdoutContent: stdoutBuf.String(),
+		StderrContent: stderrBuf.String(),
+	}, err
+}
