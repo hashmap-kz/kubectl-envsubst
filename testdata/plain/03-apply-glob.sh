16dcf1afb2cc844d6dbbc9a6f12ad701d340f656
alexey.zh <mailbox.stdio@gmail.com>
2025-01-04 13:49:24 +0500

    INTEGRATION-TESTS-SANDBOX - remove testdata (it was used for manual tests)

--- testdata/plain/03-apply-glob.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-(
-  cd manifests
-  kubectl envsubst apply -f '*.yaml'
-)
--- testdata/subst/basic/dry-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# include env-vars
-. vars.sh
-
-# expand manifests
-kubectl envsubst apply -f manifests/ --envsubst-allowed-prefixes=CI_,APP_,INFRA_ --dry-run=client -oyaml
