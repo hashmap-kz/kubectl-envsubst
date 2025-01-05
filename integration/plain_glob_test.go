2ddbfd4b4fa9c53a2f6aedf23c9792a29ea15191
alexey.zh <mailbox.stdio@gmail.com>
2025-01-05 16:42:51 +0500

    INTEGRATION-TESTS-V3 - add integration test with glob patterns

---
+++ integration/plain_glob_test.go
@@ -0,0 +1,57 @@
+package integration
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestEnvsubstIntegration_NoSubst_GlobPatterns_Yaml(t *testing.T) {
+
+	// if os.Getenv(integrationTestEnv) != integrationTestFlag {
+	// 	t.Log("integration test was skipped due to configuration")
+	// 	return
+	// }
+
+	printEnvsubstVersionInfo(t)
+
+	namespaceName := "kubectl-envsubst-plain-glob"
+	createNs(t, namespaceName)
+
+	// Setup context
+	setContextNs(t, namespaceName)
+
+	// Run kubectl-envsubst
+
+	cmdEnvsubstApply := exec.Command("kubectl", "envsubst", "apply", "-f", "immutable_data/resolve/plain-glob/*.yaml")
+	output, err := cmdEnvsubstApply.CombinedOutput()
+	stringOutput := string(output)
+	if err != nil {
+		t.Fatalf("Failed to run kubectl envsubst: %v, output: %s", err, stringOutput)
+	}
+	t.Logf("\n%s\n", strings.TrimSpace(stringOutput))
+
+	expectResources := []string{
+		"cm-1-level-0", // *.yaml
+		"cm-2-level-0", // *.yaml
+	}
+
+	unExpectResources := []string{
+		"cm-3-level-0", // *.json, should be ignored
+	}
+
+	for _, er := range expectResources {
+		expectedOutput := strings.Contains(stringOutput, er)
+		if !expectedOutput {
+			t.Errorf("Expected substituted output to contain '%s', got %s", er, stringOutput)
+		}
+	}
+
+	for _, er := range unExpectResources {
+		unExpectedOutput := strings.Contains(stringOutput, er)
+		if unExpectedOutput {
+			t.Errorf("Expected substituted output to contain '%s', got %s", er, stringOutput)
+		}
+	}
+
+}
