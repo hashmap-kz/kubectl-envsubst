8cecedd051572a9adf74c86739e9e01797e821a5
alexey.zh <mailbox.stdio@gmail.com>
2025-01-05 16:45:39 +0500

    INTEGRATION-TESTS-V3 - add another integration test for glob-patterns

--- integration/plain_glob_test.go
+++ integration/plain_glob_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
@@ -8,10 +9,10 @@ import (
 
 func TestEnvsubstIntegration_NoSubst_GlobPatterns_Yaml(t *testing.T) {
 
-	// if os.Getenv(integrationTestEnv) != integrationTestFlag {
-	// 	t.Log("integration test was skipped due to configuration")
-	// 	return
-	// }
+	if os.Getenv(integrationTestEnv) != integrationTestFlag {
+		t.Log("integration test was skipped due to configuration")
+		return
+	}
 
 	printEnvsubstVersionInfo(t)
 
@@ -50,7 +51,57 @@ func TestEnvsubstIntegration_NoSubst_GlobPatterns_Yaml(t *testing.T) {
 	for _, er := range unExpectResources {
 		unExpectedOutput := strings.Contains(stringOutput, er)
 		if unExpectedOutput {
+			t.Errorf("Expected substituted output does not contain '%s'", er)
+		}
+	}
+
+}
+
+func TestEnvsubstIntegration_NoSubst_GlobPatterns_Json(t *testing.T) {
+
+	if os.Getenv(integrationTestEnv) != integrationTestFlag {
+		t.Log("integration test was skipped due to configuration")
+		return
+	}
+
+	printEnvsubstVersionInfo(t)
+
+	namespaceName := "kubectl-envsubst-plain-glob"
+	createNs(t, namespaceName)
+
+	// Setup context
+	setContextNs(t, namespaceName)
+
+	// Run kubectl-envsubst
+
+	cmdEnvsubstApply := exec.Command("kubectl", "envsubst", "apply", "-f", "immutable_data/resolve/plain-glob/*.json")
+	output, err := cmdEnvsubstApply.CombinedOutput()
+	stringOutput := string(output)
+	if err != nil {
+		t.Fatalf("Failed to run kubectl envsubst: %v, output: %s", err, stringOutput)
+	}
+	t.Logf("\n%s\n", strings.TrimSpace(stringOutput))
+
+	expectResources := []string{
+		"cm-3-level-0", // *.json
+	}
+
+	unExpectResources := []string{
+		"cm-1-level-0", // *.yaml
+		"cm-2-level-0", // *.yaml
+	}
+
+	for _, er := range expectResources {
+		expectedOutput := strings.Contains(stringOutput, er)
+		if !expectedOutput {
 			t.Errorf("Expected substituted output to contain '%s', got %s", er, stringOutput)
+		}
+	}
+
+	for _, er := range unExpectResources {
+		unExpectedOutput := strings.Contains(stringOutput, er)
+		if unExpectedOutput {
+			t.Errorf("Expected substituted output does not contain '%s'", er)
 		}
 	}
 
